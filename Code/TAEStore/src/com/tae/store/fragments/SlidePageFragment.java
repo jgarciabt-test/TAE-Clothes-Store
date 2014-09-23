f3a88e016792967981fbfe46909ad335d746ab6a
Jose <galindin_cb@hotmail.com>
2014-09-23 17:19:07 +0100

    Code added

---
+++ Code/TAEStore/src/com/tae/store/fragments/SlidePageFragment.java
@@ -0,0 +1,24 @@
+package com.tae.store.fragments;
+
+
+
+import roboguice.fragment.RoboFragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.actionbarsherlock.app.SherlockFragment;
+import com.tae.store.R;
+
+public class SlidePageFragment extends SherlockFragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup) inflater.inflate(
+                R.layout.fragment_slide_page, container, false);
+
+        return rootView;
+    }
+}
