6d0a56f31750baf9885b7c8575e94e642594a1e1
Jose <galindin_cb@hotmail.com>
2014-09-24 14:50:24 +0100

    Orientatrion changes supported. Adapter parameterized

---
+++ Code/.metadata/.plugins/org.eclipse.core.resources/.history/3f/701d18a9e14300141ba6fceb06b61e61
@@ -0,0 +1,42 @@
+package com.tae.store.adapters;
+
+import java.util.ArrayList;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+import com.tae.store.fragments.SlidePageFragment;
+
+/**
+ * A simple pager adapter that represents 5 ScreenSlidePageFragment objects, in
+ * sequence.
+ */
+public class SlidePagerAdapter extends FragmentStatePagerAdapter {
+	
+	private int num_pages = 5;
+	
+	private String [] data;
+	private ArrayList<SlidePageFragment> list;
+	
+    public SlidePagerAdapter(FragmentManager fragmentManager, String[] array) {
+        super(fragmentManager);
+        
+        list = new ArrayList<SlidePageFragment>();
+        for(int i=0;i<num_pages;i++){
+        	list.add(new SlidePageFragment());
+        }
+        
+    	data = array;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return list.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return num_pages;
+    }
+}
--- Code/TAEStore/src/com/tae/store/fragments/HomeFragment.java
+++ Code/TAEStore/src/com/tae/store/fragments/HomeFragment.java
@@ -3,6 +3,7 @@ package com.tae.store.fragments;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -21,11 +22,21 @@ public class HomeFragment extends SherlockFragment {
     private ViewPager mPager2;
     private SlidePagerAdapter mAdapter1;
     private SlidePagerAdapter mAdapter2;
+    
+    
+    //TODO just for testing
+	private PageIndicator mIndicator3;
+	private PageIndicator mIndicator4;
+    private ViewPager mPager3;
+    private ViewPager mPager4;
+    private SlidePagerAdapter mAdapter3;
+    private SlidePagerAdapter mAdapter4;
 	
     
     //TODO only for testing
     private String[] first = {"Lorem", "impsum", "dos", "tres", "cuatro"};
     private String[] second = {"one", "two", "three", "for", "five"};
+    private String[] otro = {"one", "two"};
     
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -34,7 +45,7 @@ public class HomeFragment extends SherlockFragment {
         ViewGroup rootView = (ViewGroup) inflater.inflate(
                 R.layout.fragment_home, container, false);
         
-        mAdapter1 = new SlidePagerAdapter(getChildFragmentManager(), first);
+        mAdapter1 = new SlidePagerAdapter(getChildFragmentManager(), first, "#FF0000");
 
         mPager1 = (ViewPager)rootView.findViewById(R.id.vp1);
         mPager1.setAdapter(mAdapter1);
@@ -42,12 +53,26 @@ public class HomeFragment extends SherlockFragment {
         mIndicator1 = (CirclePageIndicator)rootView.findViewById(R.id.indicator1);
         mIndicator1.setViewPager(mPager1);
         
-        mAdapter2 = new SlidePagerAdapter(getChildFragmentManager(), second);
+        mAdapter2 = new SlidePagerAdapter(getChildFragmentManager(), second, "#AA2200");
         mPager2 = (ViewPager) rootView.findViewById(R.id.vp2);
         mPager2.setAdapter(mAdapter2);
         
         mIndicator2 = (CirclePageIndicator)rootView.findViewById(R.id.indicator2);
         mIndicator2.setViewPager(mPager2);
+        
+        mAdapter3 = new SlidePagerAdapter(getChildFragmentManager(), first, "#FF5511");
+        mPager3 = (ViewPager) rootView.findViewById(R.id.vp3);
+        mPager3.setAdapter(mAdapter3);
+        
+        mIndicator3 = (CirclePageIndicator)rootView.findViewById(R.id.indicator3);
+        mIndicator3.setViewPager(mPager3);
+        
+        mAdapter4 = new SlidePagerAdapter(getChildFragmentManager(), otro, "#FF00AA");
+        mPager4 = (ViewPager) rootView.findViewById(R.id.vp4);
+        mPager4.setAdapter(mAdapter4);
+        
+        mIndicator4 = (CirclePageIndicator)rootView.findViewById(R.id.indicator4);
+        mIndicator4.setViewPager(mPager4);
 
         return rootView;
 	}
