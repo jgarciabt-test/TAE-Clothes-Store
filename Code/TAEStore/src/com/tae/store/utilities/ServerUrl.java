cb9e8c3ac166875e5234b414f18bdbad041085ba
Jose <galindin_cb@hotmail.com>
2014-10-16 12:03:11 +0100

    Online server added

--- Code/TAEStore/src/com/tae/store/utilities/ServerUrl.java
+++ Code/TAEStore/src/com/tae/store/utilities/ServerUrl.java
@@ -9,7 +9,9 @@ package com.tae.store.utilities;
 public class ServerUrl {
 	
 	//public static final String BASE_URL = "http://192.168.1.4/TAE_STORE/";
-	public static final String BASE_URL = "http://10.0.16.27/TAE_STORE/";
+	//public static final String BASE_URL = "http://10.0.16.27/TAE_STORE/";
+	//public static final String BASE_URL = "http://169.254.200.36/TAE_STORE/";
+	public static final String BASE_URL = "http://37.187.99.23/tae/jose/TAE_STORE/";
 	public static final String GET_CATEGORIES = "get_categories.php?main_category=";
 	public static final String GET_ALL_PRODUCTS = "get_all_products.php?category=";
 	public static final String GET_ALL_CATEGORIES ="get_all_categories.php";
